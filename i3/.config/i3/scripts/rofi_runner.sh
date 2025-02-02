c43d757c79792fa4b72fa3522831df9b4b5a424f
ritikdoijod <ritikdoijod@gmail.com>
2025-02-02 23:15:43 +0530

    saving changes

---
+++ i3/.config/i3/scripts/rofi_runner.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# Import Current Theme
+DIR="$HOME/.config/i3"
+# RASI="$DIR/theme/rofi/runner.rasi"
+
+# Run
+rofi \
+  -show run
+# -theme ${RASI}
---
+++ nvim/.config/nvim/lua/plugins/conform.lua
@@ -0,0 +1,15 @@
+return {
+  "stevearc/conform.nvim",
+  opts = {
+    formatters_by_ft = {
+      lua = { "stylua" },
+      html = { "prettierd" },
+      css = { "prettierd" },
+      js = { "prettierd" },
+      mjs = { "prettierd" },
+      cjs = { "prettierd" },
+      javascriptreact = { "prettierd" },
+      json = { "prettierd" },
+    },
+  },
+}
