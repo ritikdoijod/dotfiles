89a58716933d280cd8c9e8b815bf2d1306282316
ritikdoijod <ritikdoijod@gmail.com>
2025-02-08 16:39:06 +0530

    updated formatters

--- nvim/.config/nvim/lua/plugins/conform.lua
+++ nvim/.config/nvim/lua/plugins/conform.lua
@@ -1,15 +1,14 @@
 return {
-  "stevearc/conform.nvim",
-  opts = {
-    formatters_by_ft = {
-      lua = { "stylua" },
-      html = { "prettierd" },
-      css = { "prettierd" },
-      js = { "prettierd" },
-      mjs = { "prettierd" },
-      cjs = { "prettierd" },
-      javascriptreact = { "prettierd" },
-      json = { "prettierd" },
-    },
-  },
+	"stevearc/conform.nvim",
+	opts = {
+		formatters_by_ft = {
+			lua = { "stylua" },
+			html = { "prettierd" },
+			css = { "prettierd" },
+			javascript = { "prettierd" },
+			javascriptreact = { "prettierd" },
+			json = { "prettierd" },
+			typescript = { "prettierd" },
+		},
+	},
 }
